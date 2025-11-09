2599387a34568a8378924b887ad1911c40f3f484
maicol_jb <64624079+maik151@users.noreply.github.com>
2025-11-08 22:33:04 -0500

    Configuracion del program.cs y el DbContext para usar entityFramework

---
+++ backend/BeaconDeskSolution/BeaconDesk.Infraestructure/Persistence/DbContext/BeaconDeskDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeaconDesk.Infraestructure.Persistence.DbContext
+{
+    internal class Class1
+    {
+    }
+}
