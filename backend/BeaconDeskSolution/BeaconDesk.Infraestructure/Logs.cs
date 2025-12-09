484b5639365128dd4a1f1e4a87dd3ec893872aa7
maicol_jb <64624079+maik151@users.noreply.github.com>
2025-12-08 19:54:12 -0500

    feat: Implementacion Logs

---
+++ backend/BeaconDeskSolution/BeaconDesk.Infraestructure/Logs.cs
@@ -0,0 +1,9 @@
+﻿namespace BeaconDesk.Infraestructure
+{
+    public class Logs
+    {
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+        
+    }
+}
