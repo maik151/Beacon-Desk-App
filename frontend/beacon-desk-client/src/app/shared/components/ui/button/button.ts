da77e4ca3fc3b7e3897f4af111ad6f9c4a34e8b2
maicol_jb <64624079+maik151@users.noreply.github.com>
2025-12-23 19:27:16 -0500

    feat: Crear comonente de tipo input

--- frontend/beacon-desk-client/src/app/shared/components/ui/button/button.ts
+++ frontend/beacon-desk-client/src/app/shared/components/ui/button/button.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-button',
-  imports: [],
-  templateUrl: './button.html',
-  styleUrl: './button.scss',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+            <button 
+                [type]="type"
+                [disabled]="disabled || loading"
+                (click)="onClick.emit($event)"
+                class="w-full flex justify-center items-center gap-2 px-4 py-3 rounded-lg font-semibold transition-all duration-200"
+                [ngClass]="getClasses()"
+              >
+                @if (loading) {
+                  <svg class="animate-spin h-5 w-5 text-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+                }
+                
+                <ng-content></ng-content>
+              </button>
+              `
 })
+
+
 export class Button {
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() variant: 'primary' | 'outline' | 'ghost' = 'primary';
+  @Input() disabled = false;
+  @Input() loading = false;
+  @Output() onClick = new EventEmitter<Event>();
+
+getClasses(){
+  const base = {
+      'primary': 'bg-primary text-text-inverse hover:bg-primary-hover disabled:opacity-70',
+      'outline': 'border border-border text-text-main hover:bg-surface-input',
+      'ghost': 'text-text-main hover:bg-surface-input'
+    };
+    return base[this.variant]; 
+}
+
 
 }
